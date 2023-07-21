a0f6bf35d4d3f0275c551a87841d22c4f3cab672
salvomc <smcrwrk@gmail.com>
2023-07-21 10:18:35 +0200

    exercise completed

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <title>laravel-first-steps</title>
+    </head>
+    <body>
+        <div class="d-flex justify-content-center">
+            <h1 class="mt-5 fs-1">{{$title}}</h1>
+        </div>
+    </body>
+</html>
